c379500bd212c0dc4da95ce91dda18d35fc77a17
Donald Hall <donnie@plasticwrapper.com>
2019-04-15 18:58:47 -0500

    WIP -- almost done switching over all the commands used.

--- pivotal_digest.rb
+++ pivotal_digest.rb
@@ -3,28 +3,46 @@
 require 'rubygems'
 require 'bundler/setup'
 
-require 'pivotal-tracker'
+# require 'pivotal-tracker'
+require 'tracker_api'
 require 'pry'
 
 token = ENV['TOKEN']
 project_id = ENV['PROJECT_ID']
 owners = ENV['OWNERS'].split(',')
 
-PivotalTracker::Client.token = token
-project = PivotalTracker::Project.find(project_id)
+client = TrackerApi::Client.new(token: token)
 
+project = client.project(project_id)
+
+# client-side env var defines these... we don't derive them from the api... ?
+# ['dh', 'sr']
 owners.each do |owner|
-  stories = project.stories.all(owner: owner, state: %w(started unstarted))
 
-  story_with_correct_owner = stories.find { |s| s.owned_by[0].downcase == owner[0] }
 
-  next unless story_with_correct_owner
+  # get all the started and unstarted Stories for this owner
+  stories = project.stories(filter: "owner:#{owner} state:started,unstarted")
 
-  puts "## #{story_with_correct_owner.owned_by}\n\n"
+  # refine the stories list... for those stories with multiple owners, whoever is first... that's who gets it in their queue for the email... 
+  # FIXME: make this work again if we need it?
+  # story_with_correct_owner = stories.find { |s| s.owned_by[0].downcase == owner[0] }
 
+  # if the first owner in the Story object is not the current owner... well then let's just stop right here... not printing anything!
+  # FIXME: put this back if we still need it.
+  # next unless story_with_correct_owner
+
+  # print out owner the name?  or the initials?
+  # puts "## #{story_with_correct_owner.owned_by}\n\n"
+  person = project.memberships.map(&:person).find { |person| person.initials.downcase == owner }
+  puts "## #{person.name}"
+
+  # get all their in_progress stories together for printing
   in_progress = project.stories.all(owner: owner, state: 'started')
+
+  # get all their unstarted stories together for printing.
   unstarted = project.stories.all(owner: owner, state: 'unstarted')
 
+  ## print out the story name, and the url.
   unless in_progress.empty?
     puts "### In progress\n\n"
     in_progress.each do |story|
@@ -43,4 +61,3 @@ owners.each do |owner|
 
   puts "\n"
 end
-
